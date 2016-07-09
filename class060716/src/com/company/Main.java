558fb52ce688553f1b7500ea8c3803c7628ca436
Avi <avi.davidov@gmail.com>
2016-07-09 22:13:49 +0300

    update

--- class060716/src/com/company/Main.java
+++ class060716/src/com/company/Main.java
@@ -3,19 +3,56 @@ package com.company;
 public class Main {
 
 
-    // x=10
-    // y=3
+
     public static int distance (int x, int y) {
 
         int d = 0;
-        while (x > y) {
-            y++;
-            d++;
+        int small=y;
+        int large=x;
+        while (small <large) {
+        d++;
         }
         return d;
 
     }
-  //  public static int multyply (int a, int b){
+    //homework 1
+    public static int distance1 (int a, int b){
+        int d;
+        for (d = 0; a > b +d ; d++){
+        };
+        return d;
+
+    }
+    //homework2
+    public static int multyply(int a, int b){
+        int m ; int count;
+        for (m = 0, count=0; count<a ; m=m+b,count++) {
+        };
+        return m;
+    }
+    //homework3
+    public static int devide (int a, int b){
+
+        if (a>b) {
+            return 0;
+        }
+        else{
+            int count; int m; int x;
+            for (count=0, m=a, x=a; a <= b ; m=m+a, count++) {
+                a=a+x;
+            }
+            return count;
+        }
+
+    }
+
+
+    public static void main (String[] args){
+
+        System.out.println(distance1 (8,3));
+        System.out.println(multyply (2,30));
+        System.out.println(devide (42,84));
+    }
 
  }
 
